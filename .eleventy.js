08cd82cbbe2e72a58552f6622e4d80c00ee477ca
Lars Bodewig <40965749+LarsBodewig@users.noreply.github.com>
2022-07-01 21:03:44 +0200

    Added non-redundant title

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,25 @@
+const prettier = require("prettier");
+
+function collectionSortTitle(collectionApi) {
+    const collection = collectionApi.getAll();
+    const colCopy = [...collection]; // prevent sorting inplace
+    const ordered = colCopy.sort((a, b) => a.data.title - b.data.title);
+    return ordered;
+}
+
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("assets");
-  return {
-    dir: {
-      input: "pages",
-      output: "build",
-    },
-    pathPrefix: "/",
-  };
+    eleventyConfig.addPassthroughCopy("assets");
+    eleventyConfig.addCollection("nav", collectionSortTitle);
+    eleventyConfig.addLiquidShortcode("titleOf", (page, collection) => {
+        const colPage = collection.find(elem => elem.url === page.url);
+        return colPage.data.title;
+    });
+
+    return {
+        dir: {
+            input: "pages",
+            output: "build",
+        },
+        pathPrefix: "/",
+    };
 };
